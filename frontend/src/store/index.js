f5f5fa860a4aba779db3787bc4107a08efea2dff
SAMKIEL <samuelezekiel488@gmail.com>
2025-11-19 10:24:44 +0100

    `Update Redux store and provider to use relative import path`

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import questionsReducer from './slices/questionsSlice';
-import examsReducer from './slices/examsSlice';
-import dashboardReducer from './slices/dashboardSlice';
-import communityReducer from './slices/communitySlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import questionsReducer from "./slices/questionsSlice";
+import examsReducer from "./slices/examsSlice";
+import dashboardReducer from "./slices/dashboardSlice";
+import communityReducer from "./slices/communitySlice";
 
 export const store = configureStore({
   reducer: {
